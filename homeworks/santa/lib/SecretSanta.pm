3e809c60c77ca75cf546b21ba918e4fc19f3394f
Satbek <set.turg@mail.ru>
2017-03-05 00:54:58 +0300

    Add files via upload

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -1,5 +1,4 @@
 package SecretSanta;
-
 use 5.010;
 use strict;
 use warnings;
@@ -8,9 +7,34 @@ use DDP;
 sub calculate {
 	my @members = @_;
 	my @res;
-	# ...
-	#	push @res,[ "fromname", "toname" ];
-	# ...
+	my %pair;
+	my %all;
+	for (@members) {
+		if (ref $_ eq "ARRAY") {
+			$pair{$_->[0]} = $_->[1];
+			$pair{$_->[1]} = $_->[0];
+			$all{$_->[0]} = "";
+			$all{$_->[1]} = "";
+		}
+		elsif (not ref $_) {
+			$pair{$_} = "";
+			$all{$_} = "";
+		}
+		else {
+			die "wrong input\n";
+		}
+	}
+	for my $i(keys %pair) {
+		my @variants = grep {$_ ne $pair{$i} and $all{$_} eq "" and $_ ne $i} keys %all;
+		if (scalar @variants == 0) {
+			next;
+		}
+		my $key = $variants[rand(scalar @variants)];
+		$all{$key} = $i;
+	}
+	for (keys %all) {
+		push @res,[$all{$_},$_];
+	}
 	return @res;
 }
 
