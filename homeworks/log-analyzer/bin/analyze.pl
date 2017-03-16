045a07bf18a3613710f78497a4dcc52309aa9e8d
Satbek <set.turg@mail.ru>
2017-03-16 05:41:20 +0300

    govnocode

--- homeworks/log-analyzer/bin/analyze.pl
+++ homeworks/log-analyzer/bin/analyze.pl
@@ -39,12 +39,12 @@ sub parse_file {
     while (my $log_line = <$fd>) {
         # you can put your code here
         # $log_line contains line from log file
-		$log_line =~ m/^(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s*\[(.+)\]\s*"(.+?)"\s*(\d+)\s*(\d+)\s*"(.*?)"\s*"(.*?)"\s*"(\d+.\d+|.*)"/;
+		$log_line =~ m/^(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s*\[(\d{2}\/\w+\/\d{4}:\d{2}:\d{2}:\d{2} \+0300)\]\s*"(.+?)"\s*(\d+)\s*(\d+)\s*"(.*?)"\s*"(.*?)"\s*"(\d+.\d+|.*)"/;
 		my $ip = $1;
 		$logs{$ip} ||= {};
 		$logs{$ip}->{count} ||= 0;
 		$logs{$ip}->{count}++;
-		my $data = int($5);
+		my $data = $5;
 		my $status = $4;
 		$statuses{$status} = "";#to get the array of statuses
 		$logs{$ip}->{"$status"} ||= 0;
@@ -53,7 +53,7 @@ sub parse_file {
 		$logs{$ip}->{"data"} ||= 0;
 		if ($status eq "200") {
 			if ($coef ne "-") {
-				$logs{$ip}->{"data"} += $data*$coef;
+				$logs{$ip}->{"data"} += int($data*$coef);
 			}
 			else {
 				$logs{$ip}->{"data"} += $data;
@@ -69,7 +69,6 @@ sub parse_file {
 			$logs{$ip}->{"$_"} ||= 0
 		}
 	}
-	p %logs;
     # you can put your code here
 	$result = \%logs;
     return $result;
@@ -79,26 +78,43 @@ sub report {
     my $result = shift;
 	my %logs = %{$result};
 	my @a = sort {$logs{$b}->{count} <=> $logs{$a}->{count}} keys %logs;
+	for my $k(keys %logs) {
+		#03/Mar/2017:18:28:38 +0300
+		my %seen;
+		$logs{$k}->{uniq_minuets} = scalar grep {!$seen{$_}++} map {$_ =~ s/:\d{2}\s+\+0300//; $_;} @{$logs{$k}->{"times"}};
+		$logs{$k}->{avg} = $logs{$k}->{count}/$logs{$k}->{uniq_minuets};
+	}
 	my %total;
-	for my $k(0..$#a) {
+	for my $k(keys %logs) {
 		$total{count} ||= 0;
-		$total{count} += $logs{$a[$k]}->{count};
+		$total{count} += $logs{$k}->{count};
 		$total{data} ||= 0;
-		$total{data} += $logs{$a[$k]}->{data};
+		$total{data} += $logs{$k}->{data};
 		for (sort(keys %statuses)) {
 			$total{$_} ||= 0;
-			$total{$_} += $logs{$a[$k]}->{$_};
+			$total{$_} += $logs{$k}->{$_};
 		}
+		$total{minuetes} ||= [];
+		push $total{minuetes}, @{$logs{$k}->{"times"}};
 	}
-	print "IP\t\tcount\tdata\t200\t301\t302\t400\t403\t404\t408\t414\t499\t500\n";
+	my %seen;
+	$total{uniq_minuets} = scalar grep {!$seen{$_}++} @{$total{minuetes}};
+	$total{avg} = $total{count}/$total{uniq_minuets};
 	$total{data} = int($total{data}/1024);
 	for (sort(keys %statuses)) {
 			$total{$_} = int($total{$_}/1024);
 	}
-	say "total\t\t$total{count}\t$total{data}\t$total{200}\t$total{301}\t$total{302}\t$total{400}\t$total{403}\t$total{404}\t$total{408}\t$total{414}\t$total{499}\t$total{500}\t";
+	#p %logs;
+	p %total;
+	print "IP\t\tcount\tavg\tdata\t200\t301\t302\t400\t403\t404\t408\t414\t499\t500\n";
+	print "total\t\t$total{count}\t";
+	printf("%.2f\t", $total{avg});
+	print "$total{data}\t$total{200}\t$total{301}\t$total{302}\t$total{400}\t$total{403}\t$total{404}\t$total{408}\t$total{414}\t$total{499}\t$total{500}\t\n";
 	for my $i(0..9) {
 		$logs{$a[$i]}->{data} = int($logs{$a[$i]}->{data}/1024);
-		print "$a[$i]\t$logs{$a[$i]}->{count}\t$logs{$a[$i]}->{data}\t";
+		print "$a[$i]\t$logs{$a[$i]}->{count}\t";
+		printf("%.2f\t", $logs{$a[$i]}->{avg});
+		print "$logs{$a[$i]}->{data}\t";
 		for (sort(keys %statuses)) {
 			$logs{$a[$i]}->{$_} = int($logs{$a[$i]}->{$_}/1024);
 			print "$logs{$a[$i]}->{$_}\t";
